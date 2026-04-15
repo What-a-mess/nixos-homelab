1fb40f0b3f773828ba19aea9a82450eb05db728e
wamess <44860987+What-a-mess@users.noreply.github.com>
2026-04-16 00:50:30 +0800

    feat: add caddy hostname routing with mtls

--- modules/host/edge/caddy.nix
+++ modules/host/edge/caddy.nix
@@ -1,15 +1,87 @@
-{ lib, config, ... }:
+{ lib, config, pkgs, ... }:
 let
-  paths = config.homelab.edge.paths;
-  edgeEnabled = builtins.pathExists paths.caddyfile;
+  edge = config.homelab.edge;
+  paths = edge.paths;
+  serverCert = "${paths.serverPkiDir}/fullchain.pem";
+  serverKey = "${paths.serverPkiDir}/privkey.pem";
+  clientCa = "${paths.clientCaDir}/ca.pem";
+
+  renderServiceBlock = _: service:
+    let
+      siteHost = "${service.host}.${edge.domain}";
+      backend = "${service.backendHost}:${toString service.backendPort}";
+      tlsBlock =
+        if service.requireMtls then
+          ''
+             tls ${serverCert} ${serverKey} {
+               client_auth {
+                 mode require_and_verify
+                 trust_pool file ${clientCa}
+               }
+             }
+          ''
+        else
+          ''
+             tls ${serverCert} ${serverKey}
+          '';
+    in
+    ''
+      ${siteHost}:${toString edge.port} {
+      ${tlsBlock}
+        reverse_proxy ${backend}
+      }
+    '';
+
+  caddyfileText =
+    let
+      renderedServices = lib.mapAttrsToList renderServiceBlock edge.services;
+    in
+    if renderedServices == [ ] then
+      ''
+        {
+        }
+      ''
+    else
+      lib.concatStringsSep "\n" renderedServices;
+
+  generatedCaddyfile = pkgs.writeText "edge-caddyfile" caddyfileText;
 in
-lib.mkIf edgeEnabled {
+{
   services.caddy = {
     enable = true;
     configFile = paths.caddyfile;
   };
 
-  systemd.services.caddy.serviceConfig = {
-    SupplementaryGroups = [ "caddy" ];
+  systemd.services.edge-caddyfile = {
+    description = "Install generated edge Caddyfile";
+    wantedBy = [ "multi-user.target" ];
+    before = [ "caddy.service" ];
+    restartTriggers = [ generatedCaddyfile ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+
+    path = [ pkgs.coreutils ];
+
+    script = ''
+      install -d -m 0750 ${paths.caddyConfigDir}
+      install -m 0640 -g caddy ${generatedCaddyfile} ${paths.caddyfile}
+    '';
+  };
+
+  systemd.services.caddy = {
+    after = [ "edge-caddyfile.service" ];
+    requires = [ "edge-caddyfile.service" ];
+    unitConfig.ConditionPathExists = [
+      paths.caddyfile
+      serverCert
+      serverKey
+      clientCa
+    ];
+    serviceConfig = {
+      SupplementaryGroups = [ "caddy" ];
+    };
   };
 }
