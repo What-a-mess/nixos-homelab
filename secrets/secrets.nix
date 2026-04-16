36615cf728b2cf4cc3837356cd46932f86ba67d2
wamess <44860987+What-a-mess@users.noreply.github.com>
2026-04-17 00:50:22 +0800

    fix some secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,5 @@
 let
-  homelabHost = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOQSMURU/A0HfoAO6KtQ+2dRfZHUrFiY2tD/M8/g7xrw 44860987+What-a-mess@users.noreply.github.com";
+  homelabHost = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINu30TLbM5r6+ekyfP+IWrnyxLznk2cZmwPV/ItbJy2m root@homelab";
 in {
   "rsshub.env.age".publicKeys = [ homelabHost ];
   "edge-aliyun.env.age".publicKeys = [ homelabHost ];
