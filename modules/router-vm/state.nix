0e67d620c28534b11f2be2dfb47413e9adbd7cf6
wamess <44860987+What-a-mess@users.noreply.github.com>
2026-04-25 01:38:35 +0800

    some fix

--- modules/router-vm/state.nix
+++ modules/router-vm/state.nix
@@ -5,8 +5,8 @@ let
 in {
   systemd.tmpfiles.rules = [
     "d ${stateRoot} 0755 root root - -"
-    "d ${stateRoot}/mihomo 0755 root root - -"
-    "d ${stateRoot}/mihomo/run 0755 root root - -"
-    "d ${stateRoot}/mihomo/cache 0755 root root - -"
+    "d ${stateRoot}/mihomo 0755 mihomo mihomo - -"
+    "d ${stateRoot}/mihomo/run 0755 mihomo mihomo - -"
+    "d ${stateRoot}/mihomo/cache 0755 mihomo mihomo - -"
   ];
 }
